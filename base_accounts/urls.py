9deea23be00b3b58ae34bbd5b67c01ae1108af65
José Sazo <jose.sazo@gmail.com>
2016-05-25 15:28:49 -0400

    update urlpatterns
    Support for string view arguments to url() is deprecated and will be
    removed in Django 1.10.
    
    django.conf.urls.patterns() is deprecated and will be removed in Django
    1.10. Update your urlpatterns to be a list of django.conf.urls.url()
    instances instead.

--- base_accounts/urls.py
+++ base_accounts/urls.py
@@ -1,15 +1,20 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
-from base_accounts.views import LoginFormView, SignupFormView, LogoutView, UpdateEmailFormView, UpdatePasswordFormView, PostLoginRedirectView
+from base_accounts.views import LoginFormView
+from base_accounts.views import LogoutView
+from base_accounts.views import PostLoginRedirectView
+from base_accounts.views import SignupFormView
+from base_accounts.views import UpdateEmailFormView
+from base_accounts.views import UpdatePasswordFormView
+from base_accounts.views import confirm_email_address
 
 
-urlpatterns = patterns(
-    'base_accounts.views',
+urlpatterns = [
     url(r'^login/$', LoginFormView.as_view(), name='login'),
     url(r'^post-login/$', PostLoginRedirectView.as_view(), name='post_login'),
     url(r'^signup/$', SignupFormView.as_view(), name='signup'),
     url(r'^settings/email/$', login_required(UpdateEmailFormView.as_view()), name='settings_update_email'),
     url(r'^settings/password/$', login_required(UpdatePasswordFormView.as_view()), name='settings_update_password'),
-    url(r'^confirmar/(?P<token>.+)/', 'confirm_email_address', name='confirm_email_address'),
+    url(r'^confirm/(?P<token>.+)/', confirm_email_address, name='confirm_email_address'),
     url(r'^logout/$', login_required(LogoutView.as_view()), name="logout"),
-)
+]
